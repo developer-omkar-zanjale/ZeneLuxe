1ce6ab0464256ac248bd81c2a73c24faa7bc656b
Omkar Zanjale <thinkitive@TTPL-MAC-030.local>
2024-08-20 16:35:49 +0530

    Initial commit

--- ZeneLuxe/ZeneLuxeApp.swift
+++ ZeneLuxe/ZeneLuxeApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @main
 struct ZeneLuxeApp: App {
     
-    @StateObject var coreDataService = CoreDataService()
+    @StateObject var coreDataService = CoreDataService() 
     
     var body: some Scene {
         WindowGroup {
